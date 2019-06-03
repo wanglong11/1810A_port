48dca185066c01d93c47807998d9b16eda177ccc
wwl <you@example.com>
2019-06-03 10:32:20 +0000

    测试

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel1111
+                    Larave111
                 </div>
 
                 <div class="links">
